09d0d09bf83bd411892ea71732cc7d8780016985
Hano Sura <hano1705@gmail.com>
2025-07-31 17:10:54 +0200

    redoing the physical object: from particle to pendulum

--- simulator.py
+++ simulator.py
@@ -11,7 +11,7 @@ class PendulumSimulator:
     def __init__(self):
         pass
 
-    def simulateProjectile(self, particle: Particle
+    def run_simulation(self, particle: Particle
                            , propagator: MethodType|FunctionType
                            , timestep: float):
         '''
@@ -21,67 +21,62 @@ class PendulumSimulator:
                             the projectile state.  
         '''
         # initialize result lists
-        self.time = [0]
-        self.x = [particle.position[0]]
-        self.y = [particle.position[1]]
-
-        self.vx = [particle.velocity[0]]
-        self.vy = [particle.velocity[1]]
+        self.time = np.array([0], dtype=np.float32)
+        self.x = np.array([particle.x], dtype=np.float32)
+        self.y = np.array([particle.y], dtype=np.float32)
         
         # local variables for simulation
         time = 0
-        state = np.array([self.x[0], self.y[0], self.vx[0], self.vy[0]]
+        state = np.array([particle.theta, particle.w]
                          , dtype=np.float32)
         
-        func = self.single_pendulum_dynamics(state=state)_
+        func = self.single_pendulum_dynamics
 
-        while particle.position[1] >= 0:
+        while self.time[-1] <= 5:
             # update time and state
             time, state = propagator(rhsFunc=func
                                       , time=time
                                       , state=state
                                       , timestep=timestep)
             # reset particle properties
-            particle.position = [state[0], state[1]]
-            particle.velocity = [state[2], state[3]]
+            new_position = np.array([particle.r, state[0]])
+            new_velocity = np.array([0, state[1]])
+
+            particle.set_position(position=new_position, coord_sys='polar')
+            particle.set_velocity(velocity=new_velocity, coord_sys='polar')
 
             # append results to result lists
-            self.time.append(time)
-            self.x.append(state[0])
-            self.y.append(state[1])
-            self.vx.append(state[2])
-            self.vy.append(state[3])
+            self.time = np.append(self.time, time)
+            self.x = np.append(self.x, particle.x)
+            self.y = np.append(self.y, particle.y)
 
-        return (self.time, self.x, self.y, self.vx, self.vy)
+        return (self.time, self.x, self.y)
 
     def single_pendulum_dynamics(self, state: np.ndarray):
         '''
             Function defines the RHS of a single pendulum
             :param state: the present particle state
         '''
-        x0, y0, vx0, vy0 = state
+        theta, w = state
+        r = 1
+        thetaDerivative = w
+        wDerivative = - 9.82 * np.cos(theta) / r
 
-        xDerivative = vx0
-        yDerivative = vy0
-        vxDerivative = 0
-        vyDerivative = -9.82
-
-        return np.array([xDerivative,yDerivative,vxDerivative,vyDerivative]
+        return np.array([thetaDerivative, wDerivative]
                          , dtype=np.float32)
     
 if __name__ == '__main__':
-    import particle
-    import integrations
 
-    myProjectile = particle.Particle()
-    myProjectile.setParameters(position=[0,1], velocity=[1,2]
-                               , mass=1, cDrag=0.47, area=0.01)
+    my_pendulum = Particle(mass=1)
+    my_pendulum.set_position(position=np.array([1, 7*np.pi / 4])
+                            , coord_sys='polar')
+    my_pendulum.set_velocity(velocity=np.array([0, 0])
+                            , coord_sys='polar')
     
-    rkSolver = integrations.RungeKuttaIntegrator()
+    rk_solver = RungeKuttaIntegrator()
 
-    mySimulation = ProjectileSimulator()
-    mySimulation.simulateProjectile(particle=myProjectile
-                                    , propagator=rkSolver.propagateState
-                                    , particleDynamics='drag'
+    my_simulation = PendulumSimulator()
+    my_simulation.run_simulation(particle=my_pendulum
+                                    , propagator=rk_solver.propagateState
                                     , timestep=0.01)
     print('finished simulation')
